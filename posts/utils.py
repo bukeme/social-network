fd5f5c2648b8783764eeaae6f89688aaefd4b099
Bassey Ukeme <basseyukemme@gmail.com>
2023-05-29 00:10:01 +0100

    done with group functionalities

--- posts/utils.py
+++ posts/utils.py
@@ -3,14 +3,17 @@ from django.db.models import Q
 
 
 def get_feeds_queryset(request):
-	q = []
-	q1 = Post.objects.filter(
-		Q(visibility='public') | Q(user=request.user)
+	posts = Post.objects.all()
+	group_posts = posts.filter(group__isnull=False)
+	user_posts = posts.filter(group__isnull=True)
+	q1 = user_posts.filter(
+		Q(visibility='public') |
+		Q(user__pk=request.user.pk) |
+		(Q(visibility='friends') &
+		(Q(user__userprofile__followers__in=[request.user]) |
+		Q(user__userprofile__friends__in=[request.user])))
 	)
-	# q2 = SharedPost.objects.filter(
-	# 	Q(visibility='public') | Q(user=request.user)
-	# )
-	q.extend(list(q1))
-	# q.extend(list(q2))
-	print(q)
-	return q
+	q2 = group_posts.filter(group__members__in=[request.user])
+
+	q = q1.union(q2)
+	return q.order_by('-created')

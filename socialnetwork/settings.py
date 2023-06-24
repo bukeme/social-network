00203d0c9d4a309c3e6cf193e23d08aab36a3d7f
Ukeme Bassey <basseyukemme@gmail.com>
2023-06-24 19:34:21 +0100

    updated settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-adb4.up.railway.app']
 
 
 # Application definition
